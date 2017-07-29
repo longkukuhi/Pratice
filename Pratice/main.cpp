89c7ad3197e6d8e0cb5c05d76a1faae2c64a945d
longkuku97 <575114720@qq.com>
2017-07-29 16:38:21 +0800

    Initial Commit

---
+++ Pratice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Pratice
+//
+//  Created by Longkuku on 2017/7/29.
+//  Copyright © 2017年 Longkuku. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
